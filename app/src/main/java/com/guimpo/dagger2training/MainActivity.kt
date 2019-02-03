8b2c77f998099b32d13b540c5d0ddf36cd64cdf2
guimpo <nakaima.prk@gmail.com>
2019-02-03 16:21:38 -0200

    Inject two object of type User without @Singleton annotation
    In this way we create two different objects

--- app/src/main/java/com/guimpo/dagger2training/MainActivity.kt
+++ app/src/main/java/com/guimpo/dagger2training/MainActivity.kt
@@ -11,12 +11,22 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var user: User
 
+    @Inject
+    lateinit var user1: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         (application as App).component.inject(this)
 
+        // user and user1 will be two different objects
+        // User@14dc075b User@1cca166a
+        // because we didn't annotate the function provideUser
+        // with @Singleton
         Log.d("mainActivity", user.toString())
+        Log.d("mainActivity", user1.toString())
+
+
     }
 }
