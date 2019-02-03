56532f62fc2f74ccfc5599db3eaa0492e2b0f94e
guimpo <nakaima.prk@gmail.com>
2019-02-03 14:21:19 -0200

    Inject User into MainActivity

--- app/src/main/java/com/guimpo/dagger2training/MainActivity.kt
+++ app/src/main/java/com/guimpo/dagger2training/MainActivity.kt
@@ -2,11 +2,18 @@ package com.guimpo.dagger2training
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.guimpo.dagger2training.domain.User
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var user: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("mainActivity", user.toString())
     }
 }
