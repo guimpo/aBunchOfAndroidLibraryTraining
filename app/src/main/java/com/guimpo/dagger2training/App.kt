70ef018896735e25c67c5954e88beb048faf7a05
guimpo <nakaima.prk@gmail.com>
2019-02-03 00:16:42 -0200

    Implement initDagger function

--- app/src/main/java/com/guimpo/dagger2training/App.kt
+++ app/src/main/java/com/guimpo/dagger2training/App.kt
@@ -1,8 +1,13 @@
 package com.guimpo.dagger2training
 
 import android.app.Application
+import com.guimpo.dagger2training.dagger.AppComponent
+import com.guimpo.dagger2training.dagger.AppModule
+import com.guimpo.dagger2training.dagger.DaggerAppComponent
 
 class App : Application() {
+
+    lateinit var component: AppComponent
 
     override fun onCreate() {
         super.onCreate()
@@ -10,6 +15,8 @@ class App : Application() {
     }
 
     private fun initDagger() {
-
+        component = DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
     }
 }
