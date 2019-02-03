0ea84123dc176c08621899ff97c49c01b2d682e2
guimpo <nakaima.prk@gmail.com>
2019-02-02 23:17:42 -0200

    Implement provideApp in AppModule

--- app/src/main/java/com/guimpo/dagger2training/dagger/AppModule.kt
+++ app/src/main/java/com/guimpo/dagger2training/dagger/AppModule.kt
@@ -1,8 +1,13 @@
 package com.guimpo.dagger2training.dagger
 
+import com.guimpo.dagger2training.App
 import dagger.Module
+import dagger.Provides
 
 @Module
-class AppModule {
+class AppModule(var app: App) {
+
+    @Provides
+    fun providesApp(): App = app
 
 }
