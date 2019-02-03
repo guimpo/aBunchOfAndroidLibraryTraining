a8e7df4a532f56ce01c3fa9fc85b625e0b1990c8
guimpo <nakaima.prk@gmail.com>
2019-02-03 14:28:30 -0200

    Ask to dagger to inject User into MainActivity

--- app/src/main/java/com/guimpo/dagger2training/MainActivity.kt
+++ app/src/main/java/com/guimpo/dagger2training/MainActivity.kt
@@ -14,6 +14,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (application as App).component.inject(this)
+
         Log.d("mainActivity", user.toString())
     }
 }
