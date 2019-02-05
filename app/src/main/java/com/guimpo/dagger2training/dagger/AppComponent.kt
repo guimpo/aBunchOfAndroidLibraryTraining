6ae758355eff3458b39b002304cf23485755843a
guimpo <nakaima.prk@gmail.com>
2019-02-04 22:07:59 -0200

    Add package presentation, recycler view

--- app/src/main/java/com/guimpo/dagger2training/dagger/AppComponent.kt
+++ app/src/main/java/com/guimpo/dagger2training/dagger/AppComponent.kt
@@ -1,6 +1,6 @@
 package com.guimpo.dagger2training.dagger
 
-import com.guimpo.dagger2training.MainActivity
+import com.guimpo.dagger2training.presentation.MainActivity
 import dagger.Component
 import javax.inject.Singleton
 
